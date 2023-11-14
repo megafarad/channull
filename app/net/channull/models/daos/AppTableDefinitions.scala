25fb820718bf8c0a6037cea0289b45e0ae1ae4c0
Chris Carrington <chris@megafarad.com>
2023-11-14 15:24:45 -0800

    Groundwork for support for the "Surf" feature: getting a random public ChanNull

--- app/net/channull/models/daos/AppTableDefinitions.scala
+++ app/net/channull/models/daos/AppTableDefinitions.scala
@@ -43,7 +43,7 @@ trait AppTableDefinitions { self: AuthTableDefinitions =>
 
   val chanNullTableQuery = TableQuery[ChanNullTable]
 
-  val randomPublicChanNullQuery: Query[ChanNullTable, ChanNullRow, Seq] = chanNullTableQuery.sortBy(_ => random).take(1)
+  val randomPublicChanNullQuery = chanNullTableQuery.filter(_.access === ChanNullAccess.Public).sortBy(_ => random).take(1)
 
   case class ChanNullRuleRow(id: UUID, chanNullID: UUID, number: Short, rule: String, whenCreated: Instant,
                              whoCreated: UUID)
