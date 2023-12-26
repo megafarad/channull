efcb5c10f1f0e98e552030b29b23fdc1371b5d89
Chris Carrington <chris@megafarad.com>
2023-12-25 17:02:36 -0800

    Expand ChanNullService functionality
    Added several new features to ChanNullService and its implementation. Introduced methods for getting a ChanNull either by ID or name, searching ChanNulls by name content, getting a random public ChanNull and upserting a ChanNull. These additions provide more utility to manipulate and interact with ChanNull entities in the database.

--- app/net/channull/models/services/ChanNullService.scala
+++ app/net/channull/models/services/ChanNullService.scala
@@ -1,5 +1,7 @@
 package net.channull.models.services
 
+import net.channull.models._
+
 import java.util.UUID
 import scala.concurrent.Future
 
@@ -16,4 +18,45 @@ trait ChanNullService {
    * @return IDs of deleted bans
    */
   def cleanBans: Future[Seq[UUID]]
+
+  /**
+   * Gets a ChanNull by ID
+   *
+   * @param id The ID of the ChanNull
+   * @return The ChanNull, if one exists with the given ID
+   */
+  def getChanNull(id: UUID): Future[Option[ChanNull]]
+
+  /**
+   * Gets a ChanNull by name
+   *
+   * @param name  The name of the ChanNull
+   * @return The ChanNull, if one exists with the given name
+   */
+  def getChanNull(name: String): Future[Option[ChanNull]]
+
+  /**
+   * Search for ChanNulls by name contents
+   *
+   * @param nameContains  Name contents to search by
+   * @param page          The page of the output
+   * @param pageSize      The page size of the output
+   * @return              A paginated list of ChanNulls
+   */
+  def searchChanNulls(nameContains: String, page: Int, pageSize: Int): Future[Page[ChanNull]]
+
+  /**
+   * Gets a random public ChanNull
+   *
+   * @return A random public ChanNull if it exists
+   */
+  def getRandomPublicChanNull: Future[Option[ChanNull]]
+
+  /**
+   * Upserts a ChanNull
+   *
+   * @param request The request to upsert
+   * @return  The upserted ChanNull
+   */
+  def upsertChanNull(request: UpsertChanNullRequest): Future[ChanNull]
 }
--- app/net/channull/models/services/ChanNullServiceImpl.scala
+++ app/net/channull/models/services/ChanNullServiceImpl.scala
@@ -1,13 +1,15 @@
 package net.channull.models.services
 
+import net.channull.models._
 import net.channull.models.daos._
 
 import java.time.Instant
 import java.util.UUID
 import javax.inject.Inject
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 
 class ChanNullServiceImpl @Inject() (
+  chanNullDAO: ChanNullDAO,
   chanNullPostDAO: ChanNullPostDAO,
   chanNullBanDAO: ChanNullBanDAO)(implicit ec: ExecutionContext) extends ChanNullService {
 
@@ -34,4 +36,46 @@ class ChanNullServiceImpl @Inject() (
         banId => chanNullBanDAO.delete(banId).map(_ => banId)
       })
   }
+
+  /**
+   * Gets a ChanNull by ID
+   *
+   * @param id The ID of the ChanNull
+   * @return The ChanNull, if one exists with the given ID
+   */
+  def getChanNull(id: UUID): Future[Option[ChanNull]] = chanNullDAO.get(id)
+
+  /**
+   * Gets a ChanNull by name
+   *
+   * @param name The name of the ChanNull
+   * @return The ChanNull, if one exists with the given name
+   */
+  def getChanNull(name: String): Future[Option[ChanNull]] = chanNullDAO.get(name)
+
+  /**
+   * Search for ChanNulls by name contents
+   *
+   * @param nameContains Name contents to search by
+   * @param page         The page of the output
+   * @param pageSize     The page size of the output
+   * @return A paginated list of ChanNulls
+   */
+  def searchChanNulls(nameContains: String, page: Int, pageSize: Int): Future[Page[ChanNull]] =
+    chanNullDAO.search(nameContains, page, pageSize)
+
+  /**
+   * Gets a random public ChanNull
+   *
+   * @return A random public ChanNull if it exists
+   */
+  def getRandomPublicChanNull: Future[Option[ChanNull]] = chanNullDAO.getRandomPublic
+
+  /**
+   * Upserts a ChanNull
+   *
+   * @param request The request to upsert
+   * @return The upserted ChanNull
+   */
+  def upsertChanNull(request: UpsertChanNullRequest): Future[ChanNull] = chanNullDAO.upsert(request)
 }
